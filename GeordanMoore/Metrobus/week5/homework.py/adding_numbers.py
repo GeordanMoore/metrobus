543f61be15a1f53478c5e2f50d4772bfc0ca03bd
Geordan Moore <mooregd@vcu.edu>
2020-09-23 19:29:20 -0400

    commit

---
+++ GeordanMoore/Metrobus/week5/homework.py/adding_numbers.py
@@ -0,0 +1,15 @@
+import sys
+
+def add_them_all(filename):
+    sum = 0
+    f = open('a.txt', 'r')
+    line = f.readlines()
+    line.split(",")
+    for line in f:
+        sum = sum + int(line)
+    return sum
+
+if __name__ == "__main__":
+    fname = sys.argv[1]
+    print(f"Processing {fname}")
+    print(add_them_all(fname))
